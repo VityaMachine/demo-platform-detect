f710eed146d1907bc67cacecd69dfc84ae93531a
vitya_machine <vatamanenko9@gmail.com>
2023-08-04 20:17:47 +0300

    changed logic

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const platform = document.getElementById("platform-value");
 const userAgent = document.getElementById("useragent-value");
 
-platform.innerText = window.navigator.platform;
-userAgent.innerText = window.navigator.userAgent;
+function detectBrowser() {
+  let userAgent = navigator.userAgent;
+  let browserName;
+
+  if (userAgent.match(/chrome|chromium|crios/i)) {
+    browserName = "Chrome";
+  } else if (userAgent.match(/firefox|fxios/i)) {
+    browserName = "Firefox";
+  } else if (userAgent.match(/safari/i)) {
+    browserName = "Safari";
+  } else if (userAgent.match(/opr\//i)) {
+    browserName = "Opera";
+  } else if (userAgent.match(/edg/i)) {
+    browserName = "Edge";
+  } else if (userAgent.match(/android/i)) {
+    browserName = "Android";
+  } else if (userAgent.match(/iphone/i)) {
+    browserName = "iPhone";
+  } else {
+    browserName = "Unknown";
+  }
+
+  return browserName;
+}
+
+userAgent.innerText = detectBrowser();
